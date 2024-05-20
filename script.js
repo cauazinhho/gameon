f3e5d671c63e85d09d6c25f6f7a229f278ff961c
cauazinhho <emerson.cauarodrigues@gmail.com>
2024-05-20 17:21:05 -0300

    feat: menu-hamburguer feito

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let menu = document.getElementById("menu")
 let iconeBarras = document.getElementById("icone-barras")
-let iconeX = document.getElementById("#icone-x")
+let iconeX = document.getElementById("icone-x")
 
 function abrirFecharMenu() {
    
@@ -27,4 +27,49 @@ window.onresize = () => {
     menu.classList.remove("menu-fechado")
     iconeX.style.display = "inline"
     iconeBarras.style.display = "none"
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+const solicitarOrcamento = (event) => {
+    // Pegar valores do inputs
+    let valorNome = document.getElementById("campo-nome").value
+    let valorEmail = document.getElementById("campo-email").value
+    let valorDescricao = document.getElementById("campo-descricao").value
+
+    // Organizar objeto com os valores
+    let dadosForm = {
+        nome: valorNome,
+        email: valorEmail,
+        descricao: valorDescricao
+    }
+
+    // Enviar requisição para a api
+    //127.0.0.1 -> localhost
+    //Método HTTP POST - Create -> Cadastrar ou Criar
+    
+    fetch("http://127.0.0.1:3000/solicitacoes", {
+    method: "POST", 
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(dadosForm)
+    })
+    .then(resposta => { 
+        console.log(resposta)
+
+        //Limpar os campos (inputs)
+        document.querySelector("#contato form").reset()
+
+        //Mostrar alert com msg de sucesso
+        alert("Solicitação cadastrada")
+    })
+    .catch(erro => 
+        //CASO ERRO - alert com msg de erro
+        {console.error(erro)
+        alert("Erro desconhecido")
+    }) //caso tenha erro
+    
+    event.preventDefault()
+    
 }
