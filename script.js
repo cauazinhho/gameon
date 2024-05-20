46e2086d53658c49dbd854491280685fb756ccd8
cauazinhho <emerson.cauarodrigues@gmail.com>
2024-05-20 14:34:53 -0300

    feat: inicio do menu hamburger

---
+++ script.js
@@ -0,0 +1,30 @@
+let menu = document.getElementById("menu")
+let iconeBarras = document.getElementById("icone-barras")
+let iconeX = document.getElementById("#icone-x")
+
+function abrirFecharMenu() {
+   
+
+    if (menu.classList.contains("menu-fechado")) {
+        // Abrir o menu
+        menu.classList.remove("menu-fechado")
+
+        // Mostrar icone X
+        iconeX.style.display = "inline"
+
+        // Esconder icone Barras
+        iconeBarras.style.display = "none"
+
+    } else {
+        menu.classList.add("menu-fechado")
+
+        iconeX.style.display = "none"
+        iconeBarras.style.display = "inline"
+    }
+}
+
+window.onresize = () => {
+    menu.classList.remove("menu-fechado")
+    iconeX.style.display = "inline"
+    iconeBarras.style.display = "none"
+}
