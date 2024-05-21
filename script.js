7a610c6160b0eaa7c4a4006b0b8f9901ccd10d2d
cauazinhho <emerson.cauarodrigues@gmail.com>
2024-05-21 17:24:42 -0300

    feat: site finalizado

--- script.js
+++ script.js
@@ -32,21 +32,18 @@ window.onresize = () => {
 //////////////////////////////////////////////////////////////////////////////////////////
 
 const solicitarOrcamento = (event) => {
-    // Pegar valores do inputs
+    
     let valorNome = document.getElementById("campo-nome").value
     let valorEmail = document.getElementById("campo-email").value
     let valorDescricao = document.getElementById("campo-descricao").value
 
-    // Organizar objeto com os valores
+    
     let dadosForm = {
         nome: valorNome,
         email: valorEmail,
         descricao: valorDescricao
     }
 
-    // Enviar requisição para a api
-    //127.0.0.1 -> localhost
-    //Método HTTP POST - Create -> Cadastrar ou Criar
     
     fetch("http://127.0.0.1:3000/solicitacoes", {
     method: "POST", 
@@ -57,18 +54,16 @@ const solicitarOrcamento = (event) => {
     })
     .then(resposta => { 
         console.log(resposta)
-
-        //Limpar os campos (inputs)
+ 
         document.querySelector("#contato form").reset()
 
-        //Mostrar alert com msg de sucesso
         alert("Solicitação cadastrada")
     })
     .catch(erro => 
-        //CASO ERRO - alert com msg de erro
+        
         {console.error(erro)
         alert("Erro desconhecido")
-    }) //caso tenha erro
+    }) 
     
     event.preventDefault()
     
